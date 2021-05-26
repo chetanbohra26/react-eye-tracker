b438c2eb02bea8b3c16176864920165c5bf83d20
Chetan Bohra <chetanbohra26@gmail.com>
2021-05-27 00:11:21 +0530

    added rough eye tracking

--- src/App.js
+++ src/App.js
@@ -4,56 +4,72 @@ import "./App.css";
 
 class App extends Component {
 	state = {
-		x: 0,
-		y: 0,
 		eyeLeft: React.createRef(),
 		eyeRight: React.createRef(),
 		eyeBall: React.createRef(),
+		lx: 0,
+		ly: 0,
+		rx: 0,
+		ry: 0,
 		eyeBallLX: 0,
 		eyeBallLY: 0,
 		eyeBallRX: 0,
 		eyeBallRY: 0,
-		flag: false,
+		lCircle: [],
+		rCircle: [],
 	};
 	componentDidMount() {
-		/*
-		const { eyeLeft, eyeBall } = this.state;
-		console.log("left", eyeLeft.current.getBoundingClientRect());
-		console.log("ball", eyeBall.current.getBoundingClientRect());
-		const { height, width, left, right, top, bottom } =
-			eyeBall.current.getBoundingClientRect();
-		const eyeBallLX = (left + right) / 2;
-		const eyeBallLY = (top + bottom) / 2;
-		this.setState({
-			eyeBallHeight: height,
-			eyeBallWidth: width,
-			eyeBallLX,
-			eyeBallLY,
-		});
-		*/
+		this.eyeCheckLeft();
+		this.eyeCheckRight();
 	}
-	handleEye = () => {
+
+	eyeCheckLeft() {
 		const { eyeLeft } = this.state;
-		console.log(eyeLeft.current.getBoundingClientRect());
-		this.setState({ flag: !this.state.flag });
-	};
+		const { top, bottom, left, right } =
+			eyeLeft.current.getBoundingClientRect();
+		const lx = (top + bottom) / 2;
+		const ly = (left + right) / 2;
+
+		//console.log("left", lx, ly);
+
+		this.setState({ lx, ly });
+	}
+	eyeCheckRight() {
+		const { eyeRight } = this.state;
+		const { top, bottom, left, right } =
+			eyeRight.current.getBoundingClientRect();
+		const rx = (top + bottom) / 2;
+		const ry = (left + right) / 2;
+
+		//console.log("right", rx, ry);
+
+		this.setState({ rx, ry });
+	}
+
 	handleMouseMove = (e) => {
-		/*
 		const x = e.screenX;
 		const y = e.screenY;
 		//const x = e.nativeEvent.offsetX;
 		//const y = e.nativeEvent.offsetY;
 		const { x: prevX, y: prevY } = this.state;
 		if (x === prevX && y === prevY) return;
-		console.log(x, y);
-		this.setState({ x, y });
-		*/
+
+		//console.log(x, y);
+
+		const { lx, ly } = this.state;
+		const eyeBallLX = lx > x ? "-1.5rem" : "1.5rem";
+		const eyeBallLY = ly > y ? "-1.5rem" : "1.5rem";
+
+		const { rx, ry } = this.state;
+		const eyeBallRX = rx > x ? "-1.5rem" : "1.5rem";
+		const eyeBallRY = ry > y ? "-1.5rem" : "1.5rem";
+
+		this.setState({ eyeBallLX, eyeBallLY, eyeBallRX, eyeBallRY });
 	};
 	render() {
 		return (
 			<div className="main-container">
 				<h1 className="main-nav">Hey</h1>
-				<button onClick={this.handleEye}>Pick</button>
 				<div
 					className="main-content"
 					onMouseMove={this.handleMouseMove}
@@ -62,29 +78,24 @@ class App extends Component {
 						<div className="person-head">
 							<div className="person-forehead"></div>
 							<div className="person-midface">
-								<div
-									className="face-eye"
-									ref={this.state.eyeBall}
-								>
+								<div className="face-eye">
 									<div
-										className={
-											"eye-ball" +
-											(this.state.flag
-												? " left"
-												: " right")
-										}
+										className="eye-ball"
 										ref={this.state.eyeLeft}
+										style={{
+											left: this.state.eyeBallLX,
+											top: this.state.eyeBallLY,
+										}}
 									></div>
 								</div>
 								<div className="face-eye">
 									<div
-										className={
-											"eye-ball" +
-											(this.state.flag
-												? " left"
-												: " right")
-										}
+										className="eye-ball"
 										ref={this.state.eyeRight}
+										style={{
+											left: this.state.eyeBallRX,
+											top: this.state.eyeBallRY,
+										}}
 									></div>
 								</div>
 							</div>
